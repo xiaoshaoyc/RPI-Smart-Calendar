7214174310c9944b827184480c2ebbcdb387fdf8
wangz45@rpi.edu <wangz45@rpi.edu>
2021-07-12 23:16:20 +0800

    123

--- backend/RPI_Smart_Calendar/Calendar/admin.py
+++ backend/RPI_Smart_Calendar/Calendar/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event, Day, Week
+
+
+class EventInline(admin.TabularInline):
+    model = Event
+    extra = 3
+
+class DayAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     (None,               {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    # ]
+    inlines = [EventInline]
+    # list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # list_filter = ['pub_date']
+    # search_fields = ['question_text']
+admin.site.register(Day, DayAdmin)
+
+class DayInline(admin.TabularInline):
+    model = Day
+    extra = 7
+
+class WeekAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     (None,               {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    # ]
+    inlines = [DayInline]
+    # list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # list_filter = ['pub_date']
+    # search_fields = ['question_text']
+
+admin.site.register(Week, WeekAdmin)
--- backend/RPI_Smart_Calendar/Calendar/models.py
+++ backend/RPI_Smart_Calendar/Calendar/models.py
@@ -1,3 +1,45 @@
+import datetime
+from django.utils.translation import gettext_lazy as _
 from django.db import models
-
+from django.utils import timezone
+from django.views.generic import detail
+import django
 # Create your models here.
+class Week(models.Model):
+    week_number = models.BigAutoField(primary_key=True)
+    def __str__(self):
+        return 'week'+str(self.week_number)
+
+class Day(models.Model):
+    class days(models.IntegerChoices):
+        SUN = 1, _('Sunday')
+        MON = 2, _('Monday')
+        TUE = 3, _('Tuesday')
+        WED = 4, _('Wednesday')
+        THU = 5, _('Thursday')
+        FRI = 6, _('Friday')
+        SAT = 7, _('Saturday')
+    week = models.ForeignKey(Week, on_delete=models.CASCADE)
+    day_number = models.IntegerField(choices=days.choices)
+    def __str__(self):
+        return 'day'+str(self.day_number)
+
+class Event(models.Model):
+    class methods(models.TextChoices):
+        sync = 'sync'
+        manually = 'man'
+    class types(models.TextChoices):
+        block = 'block'
+        line = 'line'
+    day = models.ForeignKey(Day, on_delete=models.CASCADE,default='')
+    title = models.CharField(max_length=50)
+    addTime = models.DateTimeField(auto_now_add=True)
+    stratTime = models.DateTimeField('start Time')
+    endTime = models.DateTimeField('end Time',default=django.utils.timezone.now)
+    method = models.CharField(max_length=4,choices=methods.choices)
+    type = models.CharField(max_length=6,choices=types.choices)
+    details = models.CharField(max_length=200)
+    estTime = models.IntegerField(default=0)
+    group = models.CharField(max_length=50, default='')
+    def __str__(self):
+        return self.title
