efdd5c061614cb9b1a710f65863e5accac0ad031
Shengzhe Sui <Shengzhe Sui>
2021-07-14 23:02:16 +0800

    1

--- backend/RPI_Smart_Calendar/User/urls.py
+++ backend/RPI_Smart_Calendar/User/urls.py
@@ -5,13 +5,7 @@ from . import views
 app_name = 'Calendar'
 
 urlpatterns = [
-    # path('login/', views.LoginView.as_view(), name='login'),
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
-
-    # path('', views.SettingsBackend, name='Backend'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('login/auth',views.login, name = "Login"),
-    # path('login/auth', views.authenticate, name='authenticate'),
-    # path('login/', views.get_user, name='get_user'),
-    # path('login/', views.get_course, name='get_course'),
+    path('login/auth', views.authenticate, name='authenticate'),
+    path('group/',views.get_course, name = "Courses"),
+    path('register/',views.register, name = "Register")
 ]
