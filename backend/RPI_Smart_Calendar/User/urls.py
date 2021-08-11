fa8085567e521f471505ba5a15778b10b3d86b28
wzy0709 <wzy0709@Outlook.com>
2021-08-11 13:59:53 +0800

    user regiser page

--- backend/RPI_Smart_Calendar/User/urls.py
+++ backend/RPI_Smart_Calendar/User/urls.py
@@ -8,6 +8,6 @@ app_name = 'User'
 urlpatterns = [
     path('', views.Logout.as_view(), name='logout'),
     path('auth/', views.Authenticate.as_view(), name='authenticate'),
-    path('register/', views.Register.as_view(), name='register'),
+    path('register/', views.register_request, name='register'),
 
 ]
